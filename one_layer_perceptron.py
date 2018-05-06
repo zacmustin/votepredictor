7673dedc1be7de94661e6c87689a0aefe088df4b
zacmustin <zachary_mustin18@milton.edu>
2018-05-06 19:31:50 -0400

    Minor edits

--- one_layer_perceptron.py
+++ one_layer_perceptron.py
@@ -8,20 +8,14 @@ import sys
 import piexif, piexif.helper, json
 from os import listdir, mkdir
 from os.path import isfile, join, exists, isdir
-"""
-def load_data(filepath):
-	data = ""
-	with open(filepath,'r') as csvfile:
-		data = csvfile
-		#for rows in csvfile:
-		#	data.append(rows.split(','))
 
-	return data
-"""
-with open('CountyFast.csv','r') as csvfile:
-	data = []
-	for rows in csvfile:
-		row_data = rows.split(',')
-		row_data[3] = row_data[3][:1]
-		data.append(row_data)
-	pprint(data)
+def load_data(filepath):
+	with open(filepath,'r') as csvfile:
+		data = []
+		for rows in csvfile:
+			row_data = rows.split(',')
+			row_data[3] = row_data[3][:1]
+			data.append(row_data)
+		return data
+
+pprint(load_data('CountyFast.csv'))
