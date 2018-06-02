04dd0c959fff16644960c75cb0daa6ee04335ada
zacmustin <zachary_mustin18@milton.edu>
2018-06-02 16:50:42 -0400

    Saving predictions and training

--- foodreader.py
+++ foodreader.py
@@ -40,10 +40,28 @@ with open('CountyLocal.csv','r') as localler: #county and fastfood breakdown
 	h = csv.reader(localler, delimiter=',')
 	next(localler)
 	for row_num in h: #go through all rows in localler
-		if row_num[41] == "":
-			local.append('0') #51 empty data points
-		else:
-			local.append(row_num[41]) #num of vegetable farms (2012)
+		print(row_num[41])
+		try: #categorizes data into groups
+			if row_num[41] == '0':
+				local.append('0')
+			elif float(row_num[41]) < 5 and float(row_num[41]) > 0:
+				local.append('1')
+			elif float(row_num[41]) > 4 and float(row_num[41]) < 10:
+				local.append('2')
+			elif float(row_num[41]) > 9 and float(row_num[41]) < 20:
+				local.append('3')
+			elif float(row_num[41]) > 19 and float(row_num[41]) < 40:
+				local.append('4')
+			elif float(row_num[41]) > 39 and float(row_num[41]) < 75:
+				local.append('5')
+			elif float(row_num[41]) > 74:
+				local.append('6')
+			elif float(row_num[41]) == "":
+				local.append('7') #51 empty data points
+			else:
+				print("Whack", row_num[41]) #num of vegetable farms (2012)
+		except:
+			print("MIZ")
 
 with open('CountyHealth.csv','r') as healthy:
 	l = csv.reader(healthy, delimiter=',')
--- tf_mlp_3.py
+++ tf_mlp_3.py
@@ -12,18 +12,21 @@ import tensorflow as tf
 from random import shuffle
 import matplotlib.pyplot as plt
 import matplotlib
+import pickle
 
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0001
 BATCH_SIZE = 260 
 COUNTIES = 2600
 NUM_BATCH = COUNTIES/BATCH_SIZE
 HL_SIZE = 100
 INPUT_SIZE = 5
 
+state_to_num = {}
+
 def load_data	(filepath):
     with open(filepath,'r') as csvfile:
         data = []
-        state_dict = {}
+        county_names = []
 
         ffr = []
         ffrpc = []
@@ -47,6 +50,7 @@ def load_data	(filepath):
             row_data = rows.split(',')
             row_data[7] = row_data[7][:1] #removes /n at end
             data.append(row_data)
+            county_names.append(data[counter][0])
 
             #loads in training data
             if counter < COUNTIES:
@@ -57,10 +61,10 @@ def load_data	(filepath):
                 low.append(float(data[counter][6]))
 
                 #Checks if state has been mapped to dict & maps if it hasn't
-                if data[counter][1] not in state_dict:
-                    state_dict.update({data[counter][1] : state_counter})
+                if data[counter][1] not in state_to_num:
+                    state_to_num.update({data[counter][1] : state_counter})
                     state_counter+=1
-                state.append(float(state_dict[data[counter][1]]))
+                state.append(float(state_to_num[data[counter][1]]))
 
                 output_list.append(data[counter][7]) #output_list = D/R
                 counter+=1
@@ -72,22 +76,24 @@ def load_data	(filepath):
                 T_rec.append(float(data[counter][5]))
                 T_low.append(float(data[counter][6]))
                 #Checks if state has been mapped to dict & maps if it hasn't
-                if data[counter][1] not in state_dict:
-                    state_dict.update({data[counter][1] : state_counter})
+                if data[counter][1] not in state_to_num:
+                    state_to_num.update({data[counter][1] : state_counter})
                     state_counter+=1
-                T_state.append(float(state_dict[data[counter][1]]))
+                T_state.append(float(state_to_num[data[counter][1]]))
 
                 T_output_list.append(data[counter][7]) #output_list = D/R
                 counter+=1
-        return array(ffr),array(ffrpc),array(state),array(veg),array(rec),array(low),array(output_list), array(T_ffr),array(T_ffrpc),array(T_state),array(T_veg),array(T_rec),array(T_low),array(T_output_list) 
+        return array(county_names),array(ffr),array(ffrpc),array(state),array(veg),array(rec),array(low),array(output_list), array(T_ffr),array(T_ffrpc),array(T_state),array(T_veg),array(T_rec),array(T_low),array(T_output_list) 
 
 ###########################################################
 #################Load in Training Data#####################
 ###########################################################
 
 # input dataset
-ffr,ffrpc,states,veg,rec,low,Y,T_ffr,T_ffrpc,T_states,T_veg,T_rec,T_low,T_Y = load_data('CountyFast.csv')
+county_names,ffr,ffrpc,states,veg,rec,low,Y,T_ffr,T_ffrpc,T_states,T_veg,T_rec,T_low,T_Y = load_data('CountyFast.csv')
 #put in ffr per capita AND ffr
+
+num_to_state = {v: k for k, v in state_to_num.items()}
 
 # CONVERSION OF INPUTS
 new_X = []
@@ -125,6 +131,16 @@ T_Y = np.array(T_new_Y)
 # tf Graph input
 pX = tf.placeholder("float", [None, INPUT_SIZE])
 pY = tf.placeholder("float", [None, 2])
+
+###########################################################
+####################Load in ALL Data#######################
+###########################################################
+
+totalX = (new_X + T_new_X)
+for county in range(len(totalX)):
+    #totalX[county] = [county_names[county]] + totalX[county]
+    totalX[county]= ([county_names[county]] + totalX[county])
+totalY = np.array(new_Y + T_new_Y)
 
 
 def perceptron(x, weights, biases):
@@ -171,6 +187,7 @@ print('Synapses successfully loaded')"""
 
 acost = []
 acc = []
+l_rate_changer = 0
 
 with tf.Session() as sess:
     sess.run(init)
@@ -188,14 +205,16 @@ with tf.Session() as sess:
             avg_cost += c/BATCH_SIZE
         # Display logs per epoch step
         acost.append(avg_cost)
+
         if epoch % 10 == 0:
             # Test model
             pred = tf.nn.softmax(logits)  # Apply softmax to logits
             correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(pY, 1))
             #print(pred)
-            # Calculate accuracy
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-            print("Epoch:", '%04d' % (epoch), "| Cost={:.5f}".format(avg_cost), "| Testing Accuracy={:.2%}".format(accuracy.eval({pX: T_X, pY: T_Y})))
+            # Calculate acc_evaluator
+            acc_evaluator = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+            accuracy = acc_evaluator.eval({pX: T_X, pY: T_Y})
+            print("Epoch:", '%04d' % (epoch), "| Cost={:.5f}".format(avg_cost), "| Testing Accuracy={:.2%}".format(accuracy))
             
             try:
                 #Graphing Cost
@@ -205,17 +224,49 @@ with tf.Session() as sess:
                 plt.clf()
 
                 #Graphing Accuracy
-                acc.append(accuracy.eval({pX: T_X, pY: T_Y}))
+                acc.append(accuracy)
                 tens_iters = [j for j in range(len(acc))]
                 for i in range(len(tens_iters)):
                     tens_iters[i]*=10
                 #print(tens_iters*10)
                 plt.scatter(tens_iters, acc, np.pi * (1/2), 'blue', alpha=0.5)
-                plt.savefig('accuracy')
+                plt.savefig('acc_evaluator')
             except:
                 print('whoops')
+
+        if epoch % 50000 == 0:
+            predictions = []
+            pred = tf.nn.softmax(logits)  # Apply softmax to logits
+            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(pY, 1))
+            # Calculate accuracy
+            acc_evaluator = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+            for county in range(len(totalX)):
+                tempX = np.array([totalX[county][1:]])
+                tempY = np.array([totalY[county]])
+                accuracy = acc_evaluator.eval({pX: tempX, pY: tempY})
+                if np.all(totalY[county] == [0,1]): #if model guesses R
+                    if accuracy == 1: #and model guessed right
+                        predictions.append([totalX[county][0], num_to_state[totalX[county][3]], 'R', 'Correct']) #append R
+                    else: #if model guessed wrong
+                        predictions.append([totalX[county][0], num_to_state[totalX[county][3]], 'D', 'Incorrect']) #append D
+                else: #if model guessed D
+                    if accuracy == 1: #if model guessed right
+                        predictions.append([totalX[county][0], num_to_state[totalX[county][3]], 'D', 'Correct']) #append D
+                    else: #if model guessed wrong
+                        predictions.append([totalX[county][0], num_to_state[totalX[county][3]], 'R', 'Incorrect']) #append R
+            pprint(predictions)
+            with open('predictions.pickle', 'wb') as f:
+                pickle.dump(predictions, f)
+
+        LEARNING_RATE = 0.0001 #resets LEARNING_RATE
+
+        if epoch % 1000 == 0:
+            if l_rate_changer == accuracy:
+                LEARNING_RATE = 0.001 #kickstarts neural network out of local minimum if accuracy stays the same for 1000 epochs
+                print("Kickstarting")
+            else:
+                l_rate_changer = accuracy
 #NEXT STEPS
-    # START RUNNING WITH CATEGORIZATION OF VARIABLES & DECREASED LEARNING RATE ON MABEAST
     # DO A BIT OF WORK INTO CHANGE - SEE IF IT'S IMPORTANT
     # MAP WRONGLY GUESSED STATES & RESEARCH NEWS STORIES/TRENDS
     # LOOK AT STATES HILLARY EXPECTED TO WIN & COMPARE MY PREDICTIONS TO IT
